814b857d019513a980079c8a497f429da8d0c5bd
syrga <kary6ekov.s@gmail.com>
2023-09-13 12:08:13 +0200

    Курсовая работа Повышенная сложность готово

--- courseeWork/src/Employee.java
+++ courseeWork/src/Employee.java
@@ -59,6 +59,7 @@ public class Employee {
                 + "salary: " + this.salary + ", " + "id; " + this.id;
     }
 
+
     public static void printEmployees(Object o) {
         Employee[] employees = (Employee[]) o;
         for (Employee element : employees) {
@@ -66,7 +67,7 @@ public class Employee {
         }
     }
 
-    public static double sumSalary(Object[] o) {
+    public static double returnSumSalary(Object[] o) {
         double sumSalary = 0;
         Employee[] employees = (Employee[]) o;
         for (Employee element : employees) {
@@ -75,22 +76,24 @@ public class Employee {
         return sumSalary;
     }
 
-    public static Object minSalary(Object[] o) {
+//    находить человека с минимальным Salary
+    public static Object findMinSalary(Object[] o) {
         Employee[] employees = (Employee[]) o;
         Arrays.sort(employees, Comparator.comparing(Employee :: getSalary));
         return employees[0];
     }
 
 
-    public static Object maxSalary(Object[] o) {
+//    находить человека с максимальным Salary
+    public static Object findMaxSalary(Object[] o) {
         Employee[] employees = (Employee[]) o;
         Arrays.sort(employees, Comparator.comparing(Employee :: getSalary));
         return employees[employees.length - 1];
     }
 
-    public static double returnAverageValue(Object[] o) {
+    public static double findAverageValue(Object[] o) {
         Employee[] employees = (Employee[]) o;
-        return sumSalary(employees) / employees.length - 1;
+        return returnSumSalary(employees) / employees.length - 1;
     }
 
     public static void printName(Object[] o) {
@@ -99,6 +102,143 @@ public class Employee {
             System.out.println(element.getEmployeeName());
         }
     }
+
+//    Повышает зарплату на заданный процент
+    public static void increaseSalary(Object[] o, int percent) {
+        Employee[] employees = (Employee[]) o;
+        for (Employee element : employees) {
+            element.salary += element.salary * ((double) percent / 100);
+        }
+    }
+
+//    Сортирует массив на основе отдела
+    public static Object sortObjectInDepartment(Object[] o) {
+        Employee[] employees = (Employee[]) o;
+        Arrays.sort(employees, Comparator.comparing(Employee :: getDepartment));
+        return employees;
+    }
+
+//    находить человека с минимальным Salary внутри отдела
+    public static Object findMinSalaryInDepartment(Object[] o, int department) {
+        Employee[] employees = (Employee[]) sortObjectInDepartment(o);
+
+        Employee minSalaryEmployee = null;
+        int nextIndex = 1;
+
+        for (Employee element : employees) {
+            if (element.department == department && element.salary < employees[nextIndex].salary) {
+                minSalaryEmployee = element;
+                nextIndex ++;
+            }
+        }
+        return minSalaryEmployee;
+    }
+
+//    находить человека с максимальным Salary внутри отдела
+    public static Object findMaxSalaryInDepartment(Object[] o, int department) {
+        Employee[] employees = (Employee[]) sortObjectInDepartment(o);
+
+        Employee maxSalaryEmployee = null;
+        int nextIndex = 1;
+        for (Employee element : employees) {
+            if (element.department == department && element.salary > employees[nextIndex].salary) {
+               maxSalaryEmployee = element;
+               nextIndex ++;
+            }
+        }
+        return maxSalaryEmployee;
+    }
+
+//    Суммa затрат на зарплату по отделу
+    public static double calculateSumSalaryDepartment(Object[] o, int department) {
+        Employee[] employees = (Employee[]) sortObjectInDepartment(o);
+
+        double sum = 0;
+        for (Employee element : employees) {
+            if (element.department == department) {
+                sum += element.salary;
+            }
+        }
+        return sum;
+    }
+
+    //    средняя зарплата по отделу
+    public static double calculateAverageValueDepartment(Object[] o, int department) {
+        Employee[] employees = (Employee[]) sortObjectInDepartment(o);
+        int count = 0;
+
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].department == department) {
+                count++;
+            }
+//          Останавливает цикл если не треубется его дальнейшая работа
+            if (employees[i].department == department && i + 1 < employees.length - 1 &&
+                    employees[i + 1].department != department ) {
+                break;
+            }
+            
+        }
+        
+        return calculateSumSalaryDepartment(o, department) / count;
+    }
+
+//    Проиндексировать зарплату всех сотрудников отдела
+    public static void increaseDepartmentSalary(Object[] o, int department, int percent) {
+        Employee[] employees = (Employee[]) sortObjectInDepartment(o);
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].department == department) {
+                employees[i].salary += employees[i].salary * ((double) percent / 100);
+            }
+//            Останавливает цикл если не треубется его дальнейшая работа
+            if (employees[i].department == department && i + 1 < employees.length - 1 &&
+                    employees[i + 1].department != department ) {
+                break;
+            }
+        }
+    }
+
+//    печатает всех сотрудников отдела
+    public static void PrintDepartmentEmployee(Object[] o, int department) {
+        Employee[] employees = (Employee[]) sortObjectInDepartment(o);
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println(1);
+            if (employees[i].department == department) {
+                System.out.println("Name: " + employees[i].getEmployeeName() + ", salary: " + employees[i].getSalary()
+                        + ", id: " + employees[i].getId());
+            }
+
+//            Останавливает цикл если не треубется его дальнейшая работа
+            if (employees[i].department == department && i + 1 < employees.length - 1 &&
+                    employees[i + 1].department != department ) {
+                break;
+            }
+        }
+
+    }
+
+//    вывести в консоль всех сотрудников с зарплатой меньше числа
+    public static void printEmployeeLessSalary(Object[] o, int number) {
+        Employee[] employees = (Employee[]) o;
+        for (Employee element : employees) {
+            if (element.salary < number) {
+                System.out.println("Name; " + element.getEmployeeName() + ", salary: " + element.getSalary()
+                        + ", id: " + element.getId());
+            }
+        }
+    }
+
+//    вывести в консоль всех сотрудников с зарплатой больше числа
+    public static void printEmployeeMoreSalary(Object[] o, int number) {
+        Employee[] employees = (Employee[]) o;
+        for (Employee element : employees) {
+            if (element.salary > number) {
+                System.out.println("Name; " + element.getEmployeeName() + ", salary: " + element.getSalary()
+                        + ", id: " + element.getId());
+            }
+        }
+    }
+
+
 
 
 
--- courseeWork/src/Main.java
+++ courseeWork/src/Main.java
@@ -3,23 +3,35 @@ public class Main {
 
         System.out.println("Hello world!");
         Employee[] employees = new Employee[7];
-        employees[0] = new Employee("Карыбеков Сыргак Карыбекович", 1, 100_000);
-        employees[1] = new Employee("Лавров Александр Сергеевич", 2, 120_000);
-        employees[2] = new Employee("Султанов Аяр Жаныбекович", 4, 60_000);
+        employees[0] = new Employee("Карыбеков Сыргак Карыбекович", 4, 100_000);
+        employees[1] = new Employee("Лавров Александр Сергеевич", 4, 120_000);
+        employees[2] = new Employee("Султанов Аяр Жаныбекович", 4, 140_000);
         employees[3] = new Employee("john Dept", 5, 80_000);
         employees[4] = new Employee("Бексултанова Альбина Жоробековна", 3, 98_034);
-        employees[5] = new Employee("Лаврова Любовь Михайловна", 4, 150_666);
+        employees[5] = new Employee("Лаврова Любовь Михайловна", 4, 50_666);
         employees[6] = new Employee("Sarah Snook", 2, 123_450);
 
         for (Employee element : employees) {
             element.getId();
         }
 
+
+
         Employee.printEmployees(employees);
-        System.out.println(Employee.sumSalary(employees));
-        System.out.println("Сотрудник с минимальной зарплатой: " + Employee.minSalary(employees));
-        System.out.println("max salary: " + Employee.maxSalary(employees));
-        System.out.println("Среднее значение зарплат: " + Employee.returnAverageValue(employees));
+        System.out.println(Employee.returnSumSalary(employees));
+        System.out.println("Сотрудник с минимальной зарплатой: " + Employee.findMinSalary(employees));
+        System.out.println("max salary: " + Employee.findMaxSalary(employees));
+        System.out.println("Среднее значение зарплат: " + Employee.findAverageValue(employees));
         Employee.printName(employees);
+
+//        Повышенная сложность
+        Employee.increaseSalary(employees, 15);
+        System.out.println("After increase salary: ");
+        Employee.printEmployees(employees);
+        System.out.println("min in department" + Employee.findMinSalaryInDepartment(employees, 4));
+        System.out.println("max in department" + Employee.findMaxSalaryInDepartment(employees, 4));
+
+        Employee.increaseDepartmentSalary(employees, 5, 12);
+        Employee.PrintDepartmentEmployee(employees, 5);
     }
 }
